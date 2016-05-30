1b548644efc1e30bcdae45a572da061f280e00b6
gauresh31 <gauresh_kambli31@ymail.com>
2016-05-30 16:15:24 +0530

    ok

--- app/src/main/java/in/mol/fragments/FragmentBasicDetails.java
+++ app/src/main/java/in/mol/fragments/FragmentBasicDetails.java
@@ -812,6 +812,7 @@ public class FragmentBasicDetails extends Fragment implements View.OnClickListen
             final Calendar c = Calendar.getInstance();
             int year = c.get(Calendar.YEAR);
             int month = c.get(Calendar.MONTH);
+
             int day = c.get(Calendar.DAY_OF_MONTH);
 
             // Create a new instance of DatePickerDialog and return it
